e0ac6d46a89447e2f9eccea2fb98ee8b92232cb0
Matheus <matheus.carvalho.sales@gmail.com>
2020-05-06 16:29:00 -0300

    adicionando Estatística

--- grafico.py
+++ grafico.py
@@ -1,7 +1,14 @@
 import matplotlib.pyplot as plt
+import json, requests
+
+response = requests.get("https://disease.sh/v2/countries/brazil")
+
+stats = json.loads(response.content)
+
+print(stats['cases'], stats['todayCases'])
 
 plt.plot([1,2,3,4] , [4,7,8,12])
 
-plt.savefig('covidGrafico.png')
+#plt.savefig('covidGrafico.png')
 
-#plt.show()
+plt.show()
