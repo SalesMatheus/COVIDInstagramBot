6270bedbb2f31929a45bb5ce6183235f2724b256
Felipe Novais <felipekun213@gmail.com>
2020-05-11 20:05:09 -0300

    Publicação de imagem

--- post.py
+++ post.py
@@ -1,9 +1,30 @@
 from instabot import Bot
 from login import *
+import os
+import time
+import random
+from os import listdir
+from os.path import isfile, join
+from random import randint
 
 bot = Bot()
 
 bot.login(username = username, password = password)
 
-bot.upload_photo("D:\Dev\Projetos\Covid\COVIDInstagramBot\img\covid.jpg",
-				caption ="Covid test")
+PhotoPath = "D:\Dev\Projetos\Covid\COVIDInstagramBot\img"
+IGCaption = "#programminglife #covid"
+os.chdir(PhotoPath)
+ListFiles = sorted([f for f in listdir(PhotoPath) if isfile(join(PhotoPath, f))])
+print(ListFiles)
+print("Total Photo in this folder:" + str(len(ListFiles)))
+
+for i, _ in enumerate(ListFiles):
+    photo = ListFiles[i]
+    print("Progress :" + str([i + 1]) + " of " + str(len(ListFiles)))
+    print("Now Uploading this photo to instagram: " + photo)
+    bot.upload_photo(photo, caption=IGCaption, upload_id=None)
+    os.remove(photo)
+    n = randint(700,900)
+    print("Sleep upload for seconds: " + str(n))
+    time.sleep(n)
+
