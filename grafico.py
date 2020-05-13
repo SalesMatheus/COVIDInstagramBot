126e6d4ba2215e6b7433a4e226839d0dc7a0dbbf
Felipe Novais <felipekun213@gmail.com>
2020-05-13 17:29:29 -0300

    Removendo bug login

--- grafico.py
+++ grafico.py
@@ -1,27 +1,32 @@
+import json
+import requests
+
 import matplotlib.pyplot as plt
-import json, requests
 
-response = requests.get("https://disease.sh/v2/countries/brazil")
+def generator ():
+    plt.figure(num=None, figsize=(5, 5), dpi=100, facecolor='w', edgecolor='k')
 
-stats = json.loads(response.content)
+    response = requests.get("https://disease.sh/v2/countries/brazil")
 
-print(stats['cases'], stats['todayCases'])
+    stats = json.loads(response.content)
 
-mes_x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+    print(stats['cases'], stats['todayCases'])
 
-casos_y = [5, 50, 100, 300, 500, 800, 2000, 6000, 6005, 6500, 7000, 8000]
+    mes_x = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
 
-plt.plot(mes_x, casos_y)
+    casos_y = [50, 500, 1000, 3000, 5000, 8000, 20000, 60000, 60050, 65000, 70000, 80000]
 
-casos_total_y = [5, 55, 155, 455, 955, 1755, 3755, 9755, 15760, 22260, 29260, 37260]
+    plt.plot(mes_x, casos_y)
 
-plt.plot(mes_x, casos_total_y)
+    casos_total_y = [5, 55, 155, 455, 955, 1755, 3755, 9755, 15760, 22260, 29260, 37260]
 
-plt.xlabel('Mês')
-plt.ylabel('Total de casos')
-plt.title('Media de casos de COVID-19')
+    plt.plot(mes_x, casos_total_y)
 
-plt.legend(['Casos no Brazil', 'Casos no mundo'])
-plt.savefig("/Users/DinoPC/PyCharmProjects/COVIDBot/BotUploads/grafico.png")
+    plt.xlabel('Mês')
+    plt.ylabel('Total de casos')
+    plt.title('Media de casos de COVID-19')
 
-plt.show()
+    plt.legend(['Casos no Brazil', 'Casos no mundo'])
+    plt.savefig("D:/Dev/Projetos/Covid/COVIDInstagramBot/img/grafico.jpg")
+
+    plt.show()
--- post.py
+++ post.py
@@ -2,10 +2,10 @@ from instabot import Bot
 from login import *
 import os
 import time
-import random
 from os import listdir
 from os.path import isfile, join
 from random import randint
+from grafico import generator
 
 bot = Bot()
 
@@ -14,17 +14,21 @@ bot.login(username = username, password = password)
 PhotoPath = "D:\Dev\Projetos\Covid\COVIDInstagramBot\img"
 IGCaption = "#programminglife #covid"
 os.chdir(PhotoPath)
+generator()
 ListFiles = sorted([f for f in listdir(PhotoPath) if isfile(join(PhotoPath, f))])
 print(ListFiles)
-print("Total Photo in this folder:" + str(len(ListFiles)))
+print("Total de fotos na pasta:" + str(len(ListFiles)))
 
 for i, _ in enumerate(ListFiles):
     photo = ListFiles[i]
-    print("Progress :" + str([i + 1]) + " of " + str(len(ListFiles)))
-    print("Now Uploading this photo to instagram: " + photo)
+    print("Progresso :" + str([i + 1]) + " de " + str(len(ListFiles)))
+    print("Carregando foto: " + photo)
     bot.upload_photo(photo, caption=IGCaption, upload_id=None)
-    os.remove(photo)
-    n = randint(700,900)
-    print("Sleep upload for seconds: " + str(n))
-    time.sleep(n)
+    os.remove(photo +'.REMOVE_ME')
+
+    # aguardar por 60 a 120s
+    n = randint(60,120)
+    print(n)
+    print("Aguardando por : " + str(n) + " segundos")
+    # time.sleep(n)
 
