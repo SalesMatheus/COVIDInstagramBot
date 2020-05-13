2e72a0ec1b1aada0e1a0cbf2d258b862542e8b9e
wagner-junior <49920681+wagner-junior@users.noreply.github.com>
2020-05-13 19:50:46 -0300

    PNG para JPG

--- dados.py
+++ dados.py
@@ -1,45 +1,26 @@
-from covid import Covid
 from PIL import Image, ImageDraw, ImageFont
-import json
+import json, requests
 
 #source
-covid = Covid(source="john_hopkins")
-
-#GlobalCases
-confirmed = covid.get_total_confirmed_cases()
-recovered = covid.get_total_recovered()
-deaths = covid.get_total_deaths()
+response = requests.get("https://disease.sh/v2/countries/brazil")
+stats = json.loads(response.content)
 
 
 #BrazilCases (getting)
-confirmedbr = json.loads(json.dumps(covid.get_status_by_country_name("Brazil")))
-getdata = confirmedbr["confirmed"]
-
-recoveredbr = json.loads(json.dumps(covid.get_status_by_country_name("Brazil")))
-getdata2 = recoveredbr["recovered"]
-
-deathsbr = json.loads(json.dumps(covid.get_status_by_country_name("Brazil")))
-getdata3 = deathsbr["deaths"]
-
-## Pillow (criando imagem) -- Dados Mundiais
-image = Image.open('/Users/DinoPC/PyCharmProjects/COVIDBot/Images/template.png')
-font_type = ImageFont.truetype('arial.ttf', 23)
-draw = ImageDraw.Draw(image)
-draw.text(xy=(170,20), text=str('Casos no Mundo'), fill=(255,69,0), font=font_type)
-draw.text(xy=(180,110), text=str(confirmed), fill=(255,69,0), font=font_type)
-draw.text(xy=(180,200), text=str(recovered), fill=(255,69,0), font=font_type)
-draw.text(xy=(180,280), text=str(deaths), fill=(255,69,0), font=font_type)
-image.show()
-image.save("/Users/DinoPC/PyCharmProjects/COVIDBot/BotUploads/worlddata.png")
+cases = stats['cases']
+todayCases = stats['todayCases']
+recovered = stats['recovered']
+deaths = stats['deaths']
 
 ## Pillow (criando imagem) -- Dados do Brasil
 image = Image.open('/Users/DinoPC/PyCharmProjects/COVIDBot/Images/template.png')
 font_type = ImageFont.truetype('arial.ttf', 23)
 draw = ImageDraw.Draw(image)
-draw.text(xy=(170,20), text=str('Casos no Brasil'), fill=(255,69,0), font=font_type)
-draw.text(xy=(180,110), text=str(getdata), fill=(255,69,0), font=font_type)
-draw.text(xy=(180,200), text=str(getdata2), fill=(255,69,0), font=font_type)
-draw.text(xy=(180,280), text=str(getdata3), fill=(255,69,0), font=font_type)
+draw.text(xy=(140,20), text=str('Casos no Brasil'), fill=(255,69,0), font=font_type)
+draw.text(xy=(160,110), text=str(f"{cases}      +{todayCases}"), fill=(255,69,0), font=font_type)
+draw.text(xy=(200,200), text=str(recovered), fill=(255,69,0), font=font_type)
+draw.text(xy=(200,280), text=str(deaths), fill=(255,69,0), font=font_type)
 image.show()
-image.save("/Users/DinoPC/PyCharmProjects/COVIDBot/BotUploads/brazildata.png")
+rgb_im = image.convert('RGB')
+rgb_im.save("/Users/DinoPC/PyCharmProjects/COVIDBot/BotUploads/brazildata.jpg")
 
